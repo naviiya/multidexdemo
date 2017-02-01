1f85211bb271bc7186ea2bb65d3ae18e041d9b72
unknown <naviiya@163.com>
2017-02-01 11:28:21 +0800

    demo
    Please enter the commit message for your changes. Lines starting

---
+++ app/src/main/java/me/tstaurus/app/multidex/MyApplication.java
@@ -0,0 +1,10 @@
+package me.tstaurus.app.multidex;
+
+import android.support.multidex.MultiDexApplication;
+
+/**
+ * Created by tstau on 2017/1/22.
+ */
+
+public class MyApplication extends MultiDexApplication {
+}
